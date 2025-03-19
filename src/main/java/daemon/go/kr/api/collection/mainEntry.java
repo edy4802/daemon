0bfbd0eed5fe32c2049759fd8874acc3cf90540f
DESKTOP-N24H8HQ\INIT-PC <saemurl@naver.com>
2025-03-19 13:34:25 +0900

    [김일규] 폴더 사용여부 Y/N 값을 Properties 에서 받아오도록 추가

--- src/main/java/daemon/go/kr/api/collection/mainEntry.java
+++ src/main/java/daemon/go/kr/api/collection/mainEntry.java
@@ -23,68 +23,80 @@ public class mainEntry {
             Scheduler scheduler = schedulerFactory.getScheduler();
             
             // 폴더01 Job & Trigger
-            JobDetail job = JobBuilder.newJob(ApiCollectionDaemon.class)
-                .withIdentity("jobName", Scheduler.DEFAULT_GROUP)
-                .build();
-            
-            Trigger trigger = TriggerBuilder.newTrigger()
-                .withIdentity("trggerName", Scheduler.DEFAULT_GROUP)
-                .withSchedule(CronScheduleBuilder.cronSchedule((String)propsMap.get("period01")))// 매월 1일 마다
-                .build();
+            if ("Y".equals(propsMap.get("use01"))) {	// Y/N 체크를 통한 폴더 사용여부 확인
+                JobDetail job = JobBuilder.newJob(ApiCollectionDaemon.class)
+                    .withIdentity("job01", Scheduler.DEFAULT_GROUP)
+                    .build();
+
+                Trigger trigger = TriggerBuilder.newTrigger()
+                    .withIdentity("trigger01", Scheduler.DEFAULT_GROUP)
+                    .withSchedule(CronScheduleBuilder.cronSchedule((String) propsMap.get("period01")))
+                    .build();
+
+                scheduler.scheduleJob(job, trigger);
+            }
             
             
             // 폴더02 Job & Trigger
-            JobDetail job2 = JobBuilder.newJob(ApiCollectionDaemon2.class) // 다른 Job 클래스
-                .withIdentity("job2", Scheduler.DEFAULT_GROUP)
-                .build();
-           
-            
-            Trigger trigger2 = TriggerBuilder.newTrigger()
-                .withIdentity("trigger2", Scheduler.DEFAULT_GROUP)
-                .withSchedule(CronScheduleBuilder.cronSchedule((String) propsMap.get("period02"))) // 다른 스케줄
-                .build();
+            if ("Y".equals(propsMap.get("use02"))) {
+                JobDetail job2 = JobBuilder.newJob(ApiCollectionDaemon2.class)
+                    .withIdentity("job02", Scheduler.DEFAULT_GROUP)
+                    .build();
+
+                Trigger trigger2 = TriggerBuilder.newTrigger()
+                    .withIdentity("trigger02", Scheduler.DEFAULT_GROUP)
+                    .withSchedule(CronScheduleBuilder.cronSchedule((String) propsMap.get("period02")))
+                    .build();
+
+                scheduler.scheduleJob(job2, trigger2);
+            }
             
             
             // 폴더03 Job & Trigger
-            JobDetail job3 = JobBuilder.newJob(ApiCollectionDaemon3.class) // 다른 Job 클래스
-                .withIdentity("job3", Scheduler.DEFAULT_GROUP)
-                .build();
-           
-            
-            Trigger trigger3 = TriggerBuilder.newTrigger()
-                .withIdentity("trigger3", Scheduler.DEFAULT_GROUP)
-                .withSchedule(CronScheduleBuilder.cronSchedule((String) propsMap.get("period03"))) // 다른 스케줄
-                .build();
+            if ("Y".equals(propsMap.get("use03"))) {
+                JobDetail job3 = JobBuilder.newJob(ApiCollectionDaemon3.class)
+                    .withIdentity("job03", Scheduler.DEFAULT_GROUP)
+                    .build();
+
+                Trigger trigger3 = TriggerBuilder.newTrigger()
+                    .withIdentity("trigger03", Scheduler.DEFAULT_GROUP)
+                    .withSchedule(CronScheduleBuilder.cronSchedule((String) propsMap.get("period03")))
+                    .build();
+
+                scheduler.scheduleJob(job3, trigger3);
+            }
+
             
             
             // 폴더04 Job & Trigger
-            JobDetail job4 = JobBuilder.newJob(ApiCollectionDaemon4.class) // 다른 Job 클래스
-                .withIdentity("job4", Scheduler.DEFAULT_GROUP)
-                .build();
-           
-            
-            Trigger trigger4 = TriggerBuilder.newTrigger()
-                .withIdentity("trigger4", Scheduler.DEFAULT_GROUP)
-                .withSchedule(CronScheduleBuilder.cronSchedule((String) propsMap.get("period04"))) // 다른 스케줄
-                .build();
+            if ("Y".equals(propsMap.get("use04"))) {
+                JobDetail job4 = JobBuilder.newJob(ApiCollectionDaemon4.class)
+                    .withIdentity("job04", Scheduler.DEFAULT_GROUP)
+                    .build();
+
+                Trigger trigger4 = TriggerBuilder.newTrigger()
+                    .withIdentity("trigger04", Scheduler.DEFAULT_GROUP)
+                    .withSchedule(CronScheduleBuilder.cronSchedule((String) propsMap.get("period04")))
+                    .build();
+
+                scheduler.scheduleJob(job4, trigger4);
+            }
             
             
             // 폴더05 Job & Trigger
-            JobDetail job5 = JobBuilder.newJob(ApiCollectionDaemon5.class) // 다른 Job 클래스
-                .withIdentity("job5", Scheduler.DEFAULT_GROUP)
-                .build();
-           
-            
-            Trigger trigger5 = TriggerBuilder.newTrigger()
-                .withIdentity("trigger5", Scheduler.DEFAULT_GROUP)
-                .withSchedule(CronScheduleBuilder.cronSchedule((String) propsMap.get("period05"))) // 다른 스케줄
-                .build();
+            if ("Y".equals(propsMap.get("use05"))) {
+                JobDetail job5 = JobBuilder.newJob(ApiCollectionDaemon5.class)
+                    .withIdentity("job05", Scheduler.DEFAULT_GROUP)
+                    .build();
+
+                Trigger trigger5 = TriggerBuilder.newTrigger()
+                    .withIdentity("trigger05", Scheduler.DEFAULT_GROUP)
+                    .withSchedule(CronScheduleBuilder.cronSchedule((String) propsMap.get("period05")))
+                    .build();
+
+                scheduler.scheduleJob(job5, trigger5);
+            }
                         
-            scheduler.scheduleJob(job, trigger);
-            scheduler.scheduleJob(job2, trigger2);
-            scheduler.scheduleJob(job3, trigger3);
-            scheduler.scheduleJob(job4, trigger4);
-            scheduler.scheduleJob(job5, trigger5);
             
             scheduler.start();
         } catch(Exception e) {
